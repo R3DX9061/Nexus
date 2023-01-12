8305f68bec918e294b14d2596872fabbd99d4309
Arseni <123@gmail.bg>
2023-01-12 11:15:55 +0200

    Mql

---
+++ Jamaikata/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Jamaikata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете число:");
+            int n = 
+        }
+    }
+}
